9d4aaf3eae7728281a02aeaddd34d358eeaa5f67
Jongwon Choi <oz.jongwon.choi@gmail.com>
2020-02-21 21:38:51 +1100

    Change child-node to accommodate heuristic-function

--- src/aima.lisp
+++ src/aima.lisp
@@ -28,6 +28,12 @@
     state action
     ))
 
+(defgeneric heuristic-function (search-type problem node action)
+  (:documentation "Heuristic function for A*. 0 for non A* algorithms")
+  (:method (search-type problem node action)
+    (declare (ignore search-type problem node action))
+    0))
+
 (defgeneric applicable-actions (problem state)
   (:documentation "ACTIONS function in AIMA."))
 #+XXX
@@ -56,13 +62,20 @@
   action
   (path-cost 0 :type fixnum))
 
-(defun child-node (problem parent action)
+(defun child-node (problem parent action &optional search-type)
+  ;; search-type: :uniform-cost, :a-star
   (with-slots ((parent-state state) path-cost)
       parent
     (make-node :state (state+action->next-state problem parent-state action)
                :parent parent
                :action action
-               :path-cost (+ path-cost (state-transition-cost problem parent-state action)))))
+               ;; For uniform-cost-search, this is g(n)
+               ;; FIXME: f(n) = g(n) + h(n) ---> A*
+               :path-cost (+ path-cost
+                             ;; g(n)
+                             (state-transition-cost problem parent-state action)
+                             ;; h(n), for non :a-star searches, 0
+                             (heuristic-function search-type problem parent action)))))
 
 (defstruct (frontier-queue (:constructor %make-frontier-queue))
   (queue nil :type (or heap fibonacci-heap fifo lifo))
@@ -133,8 +146,10 @@
 (defun depth-first-search (problem)
   (general-search problem :lifo))
 
-(defun uniform-cost-search (problem)
-  (let ((frontierq (make-frontier-queue :fibonacci-heap)))
+(defun cost-based-search (problem heap-type search-type)
+  ;; heap-type: :fibonacci-heap, :heap
+  ;; search-type: :uniform-cost, :a-star
+  (let ((frontierq (make-frontier-queue heap-type)))
     (queue-put frontierq (make-node :state (problem-initial-state problem)))
     (loop until (queue-empty? frontierq)
        as node = (queue-get frontierq)
@@ -143,13 +158,16 @@
        return (solution problem node)
        else
        do (loop for action in (applicable-actions problem (node-state node))
-             as child = (child-node problem node action)
+             as child = (child-node problem node action search-type)
              if (ready-to-explore? frontierq child)
              do (queue-put frontierq child)
              else
              do (when-let ((existing-node (peek-frontier-node-by-state frontierq (node-state child))))
                   (when (< (node-path-cost child) (node-path-cost existing-node))
                     (update-existing-frontier-node existing-node child)))))))
+
+(defun uniform-cost-search (problem heap-type)
+  (cost-based-search problem heap-type :uniform-cost))
 
 
 ;;;;;;;;;;;;;
@@ -200,7 +218,7 @@
     (ecase algorithm
       (:fifo (breadth-first-search maze))
       (:lifo (depth-first-search maze))
-      ((:fibonacci-heap :heap) (uniform-cost-search maze)))))
+      ((:fibonacci-heap :heap) (uniform-cost-search maze algorithm)))))
 
 (defmethod state+action->next-state ((problem simple-maze) state action)
     ;; action == position index
@@ -314,3 +332,36 @@
      finally (return actions)))
 
 ;;(solve-n-puzzle :fifo (n-puzzle-state '(1 2 5 3 4 0 6 7 8)))
+#+XXX
+(defparameter *romania-map-with-coordination*
+  '(
+    (Arad       ( 91 492) ((Zerind . 75) (Sibiu . 140) (Timisoara . 118)))
+    (Bucharest	(400 327) ((Fagaras . 211) (Pitesti . 101) (Giurgiu . 90)
+                           (Urziceni . 85)))
+    (Craiova	(253 288) ((Dobreta . 120) (Rimnicu . 146) (Pitesti . 138)))
+    (Dobreta	(165 299) ((Mehadia . 75) (Craiova . 120)))
+    (Eforie	(562 293) ((Hirsova . 86)))
+    (Fagaras	(305 449) ((Sibiu . 99) (Bucharest . 211)))
+    (Giurgiu	(375 270) ((Bucharest . 90)))
+    (Hirsova	(534 350) ((Urziceni . 98) (Eforie . 86)))
+    (Iasi	(473 506) ((Neamt . 87) (Vaslui . 92)))
+    (Lugoj	(165 379) ((Timisoara . 111) (Mehadia . 70)))
+    (Mehadia	(168 339) ((Lugoj . 70) (Dobreta . 75)))
+    (Neamt	(406 537) ((Iasi . 87)))
+    (Oradea	(131 571) ((Zerind . 71) (Sibiu . 151)))
+    (Pitesti	(320 368) ((Rimnicu . 97) (Craiova . 138) (Bucharest . 101)))
+    (Rimnicu	(233 410) ((Sibiu . 80) (Pitesti . 97) (Craiova . 146)))
+    (Sibiu	(207 457) ((Arad . 140) (Oradea . 151) (Fagaras . 99)
+                           (Rimnicu . 80)))
+    (Timisoara	( 94 410) ((Arad . 118) (Lugoj . 111)))
+    (Urziceni	(456 350) ((Bucharest . 85) (Hirsova . 98) (Vaslui . 142)))
+    (Vaslui	(509 444) ((Iasi . 92) (Urziceni . 142)))
+    (Zerind	(108 531) ((Arad . 75) (Oradea . 71)))
+    )
+  "A representation of the map in Figure 4.2 [p 95].
+  But note that the straight-line distances to Bucharest are NOT the same.")
+
+
+(defmethod heuristic-function ((search-type (eql :a-star)) problem parent action)
+  problem parent action
+  11)
